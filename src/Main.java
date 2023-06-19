800272fe2a2aa6469985ba43028177c4b5a20e9a
nikbatura <nikbatura@mail.ru>
2023-06-19 14:56:37 +0700

    проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
             Transport[] transports = new Transport[] {
-                    new Car("машина 1",4),
+                    new Car("машина1",4),
                     new Car("машина 2",4),
                     new Truck("грузовик 1",6),
                     new Truck("грузовик 2",8),
